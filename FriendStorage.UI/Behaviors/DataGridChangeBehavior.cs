91d904b546a7b0d0cfe40646585008b7c8aa6975
wshuts <shutswilliam@gmail.com>
2024-04-20 20:55:05 -0500

    dynamically setting error template

--- FriendStorage.UI/Behaviors/DataGridChangeBehavior.cs
+++ FriendStorage.UI/Behaviors/DataGridChangeBehavior.cs
@@ -67,7 +67,7 @@ namespace FriendStorage.UI.Behaviors
     {
       var baseStyle = dataGrid.FindResource(typeof(TextBox)) as Style;
       var style = new Style(typeof(TextBox), baseStyle);
-      AddSetters(style, bindingPath);
+      AddSetters(style, bindingPath, dataGrid);
       return style;
     }
 
@@ -75,19 +75,22 @@ namespace FriendStorage.UI.Behaviors
     {
       var baseStyle = dataGrid.FindResource("TextBlockBaseStyle") as Style;
       var style = new Style(typeof(TextBlock), baseStyle);
-      AddSetters(style, bindingPath);
+      AddSetters(style, bindingPath, dataGrid);
       return style;
     }
 
-    private static void AddSetters(Style style, string bindingPath)
+    private static void AddSetters(Style style, string bindingPath, DataGrid dataGrid)
     {
-      style.Setters.Add(new Setter(ChangeBehavior.IsActiveProperty,false));
+      style.Setters.Add(new Setter(ChangeBehavior.IsActiveProperty, false));
 
       style.Setters.Add(new Setter(ChangeBehavior.IsChangedProperty,
                                      new Binding(bindingPath + "IsChanged")));
 
       style.Setters.Add(new Setter(ChangeBehavior.OriginalValueProperty,
                                      new Binding(bindingPath + "OriginalValue")));
+
+      style.Setters.Add(new Setter(Validation.ErrorTemplateProperty,
+        dataGrid.FindResource("ErrorInsideErrorTemplate")));
     }
   }
 }
